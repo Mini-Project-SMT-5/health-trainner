a68eb35e8d1f64753dc985a646b36814bbbdbfcd
Gyu-won <milestogo0905@naver.com>
2023-08-15 14:23:29 +0900

    feat(views.py): Add dumbell curl count function

--- AI_HealthTrainer/AI_HealthTrainer/views.py
+++ AI_HealthTrainer/AI_HealthTrainer/views.py
@@ -7,6 +7,7 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
+
 mp_drawing = mp.solutions.drawing_utils # pose를 시각화해줌 (drawing utilities를 제공)
 mp_pose = mp.solutions.pose # mediapipe에서 여러가지 모델들 중 pose model을 가져옴
 
@@ -38,6 +39,7 @@ def generate_frames():
     # min_tracking_confidence: 성공적인 것으로 간주되는 포즈 추적에 대한 최소 신뢰도 점수
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose: # 값을 높일수록 더 자세히 탐지하지만 너무 정확하게 해서 아예 탐지 안될수도(trade off)
         while cap.isOpened():
+            
             ret, frame = cap.read() #frame: webcam의 image가 담김
             
             # Recolor image to RGB (opencv는 BGR을 mediapipe는 RGB를 사용해서 BGR을 RGB로 바꿔준다)
@@ -73,12 +75,28 @@ def generate_frames():
                 cv2.putText(image, str(angle), 
                             tuple(np.multiply(elbow, [640, 480]).astype(int)), 
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                    )
-                        
+                                    )                  
+            
+                # calulate counter
+                if angle > 160:
+                    stage = "down"
+                if angle < 60 and stage == "down":
+                    stage="up"
+                    curl_counter += 1  
+        
             except:
                 pass
             
-            # Render detection
+            # Render detection and curl counter
+            # setup status box (startpoint, endpoint, color, )
+            
+            # Rep data
+            cv2.putText(image, 'REPS', (15, 30), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(curl_counter),
+                        (30, 80),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 2, cv2.LINE_AA)
+            
             mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                     mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
                                     mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
@@ -86,22 +104,15 @@ def generate_frames():
             
             ret, buffer = cv2.imencode('.jpg', image)
             render_image = buffer.tobytes()
+            
+            
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + render_image+ b'\r\n')
             
-
-            
-        #     cv2.imshow("AI_Health Trainer", image) # video를 visulize 해줌 (box가 어떻게 불릴지, 보여줄 image)
-            
-        #     # to close box
-        #     if cv2.waitKey(10) & 0XFF == ord('q'):
-        #         break
-            
-        # cap.release() # quit web cam
-        # cv2.destroyAllWindows() # quit window
 
 def webcam(request):
     return StreamingHttpResponse(generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
 
 def index(request):
+    
     return render(request, 'index.html')
