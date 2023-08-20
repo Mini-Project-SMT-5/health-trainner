b621a66ba74aa7fb294aa5971268a1ae3fa6f6c9
woohyeon <6885hong@naver.com>
2023-08-20 19:25:56 +0900

    design("sjkds)

--- AI_HealthTrainer/AI_HealthTrainer/templates/javascripts/indexforCamera.js
+++ AI_HealthTrainer/AI_HealthTrainer/templates/javascripts/indexforCamera.js
@@ -1,5 +1,19 @@
 var totalSet, doneSet, mins, secs, feedback;
 
+function countdown() {
+    if (secs > 0) {
+        secs--;
+    } else if (mins > 0) {
+        mins--;
+        secs = 59;
+    } else {
+        clearInterval(interval);
+        return;
+    }
+    document.getElementById("mins").innerText = String(mins);
+    document.getElementById("seconds").innerText = String(secs);
+}
+
 fetch("") // 서버 주소
     .then(response => {
         if (!response.ok) throw new Error("Network response was not ok");
@@ -13,30 +27,17 @@ fetch("") // 서버 주소
         feedback = data.feedback;
 
         document.getElementById("set-num").innerText = doneSet + "/" + totalSet;
-        document.getElementById("mins").innerText = mins;
-        document.getElementById("seconds").innerText = secs;
+        document.getElementById("mins").innerText = String(mins);
+        document.getElementById("seconds").innerText = String(secs);
         document.getElementById("feedback").innerText = feedback;
     })
     .catch(error => {
         console.error("There was a problem with the fetch operation:", error);
+        mins = 10;
+        secs = 0;
+        const interval = setInterval(countdown, 1000);
     });
 
-document.getElementById("min-unit").addEventListener("click", function () {
-    const minE = document.getElementById("mins");
-    const secE = document.getElementById("seconds");
-    if (minE && secE) { 
-        mins = parseInt(minE.innerText);
-        secs = parseInt(secE.innerText);
-
-        if (secs > 0) secs--;
-         else {
-            mins--;
-            secs = 59;
-            minE.innerText = mins;
-        }
-        secE.innerText = secs;
-    }
-}); // 시간 변경
 
 document.getElementById("set-num").addEventListener("click", function () {
     const setE = document.getElementById("set-num");
