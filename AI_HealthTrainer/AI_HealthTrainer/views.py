a02c0766b22a6f046fe92e3cc4d454b0d535c281
Gyu-won <milestogo0905@naver.com>
2023-08-21 01:02:20 +0900

    fix(views.py, urls.py): Make connection between camera.html and time.html

--- AI_HealthTrainer/AI_HealthTrainer/views.py
+++ AI_HealthTrainer/AI_HealthTrainer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 from django.http import StreamingHttpResponse, JsonResponse
 
 from AI_HealthTrainer import health_trainer
@@ -10,12 +11,24 @@ def get_feedback(request):
     print(data)
     return JsonResponse(data)
 
+
 def video_feed(request):
     #return StreamingHttpResponse(health_trainer.generate_frames(), content_type='application/json')
     return StreamingHttpResponse(health_trainer.generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
 
+
+@csrf_protect
 def camera(request):
-    return render(request, "Structures/camera.html")
+    if request.method == 'POST':
+        set = request.POST['set']
+        reps = request.POST['reps']
+        rest = request.POST['rest']
+    return render(request, "Structures/camera.html", {'set': set, 'reps': reps, 'rest': rest})
+
+
+def time(request):
+    return render(request, "Structures/time.html")
+
 
 def index(request):
     return render(request, 'index.html')
